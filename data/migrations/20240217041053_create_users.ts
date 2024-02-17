e4fce085cb6bec279ddbea03238c1000cb515209
Jevon Cochran <jevon@securebags.com>
2024-02-17 00:42:11 -0500

    first commit

---
+++ data/migrations/20240217041053_create_users.ts
@@ -0,0 +1,19 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable("users", (tbl) => {
+    tbl.uuid("id").primary().defaultTo(knex.raw("(UUID())"));
+
+    tbl.string("firstName", 24).notNullable();
+
+    tbl.string("lastName", 24).notNullable();
+
+    tbl.string("email", 50).notNullable();
+
+    tbl.string("password", 50).notNullable();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTableIfExists("users");
+}
