1893927fdb9498dbb4473afcf1b37529bae5906e
Jevon Cochran <jevon@securebags.com>
2024-02-18 01:20:29 -0500

    create invite authentication

--- controllers/inviteController.ts
+++ controllers/inviteController.ts
@@ -1,12 +1,26 @@
 import * as inviteService from "../services/inviteService";
+import * as eventService from "../services/eventService";
 import { Request, Response } from "express";
-import { InviteInput } from "../types/custom";
+import { InviteInput, Event } from "../types/custom";
 
 // @desc Create invite
 // @route POST /api/invites
 // @access Private
 const createInvite = async (req: Request, res: Response) => {
   const { eventId, guestId, accepted, declined }: InviteInput = req.body;
+
+  // Check that event exists
+  const existingEvent: Event = await eventService.getEvent(eventId);
+
+  if (!existingEvent) {
+    return res.status(400).json({ errMsg: "Event does not exist" });
+  }
+
+  // Check that user created the event
+  // Users can only send invites for events they create
+  if (req.user.id.toString() !== existingEvent.userId) {
+    return res.status(401).json({ errMsg: "Unauthorized" });
+  }
 
   try {
     const invite = await inviteService.createInvite({
@@ -19,7 +33,7 @@ const createInvite = async (req: Request, res: Response) => {
     res.status(201).json(invite);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ errMsg: "Unable to create event" });
+    res.status(500).json({ errMsg: "Unable to send invite" });
   }
 };
 
